fcdbe49ec835238ecd653e79c33d7e9d90964818
b150005 <b150005@outlook.jp>
2025-06-07 01:52:28 +0900

    💬 メッセージに絵文字を追加

--- lib/core/utils/logging/log_message.dart
+++ lib/core/utils/logging/log_message.dart
@@ -1,11 +1,13 @@
 final class LogMessage {
   const LogMessage._();
 
-  static const firebaseNotInitialized = 'Firebase initialization missing';
+  static const firebaseNotInitialized =
+      '❌ Firebase app'
+      ' not properly initialized';
 
-  static const unhandledError = 'Unhandled error occurred';
+  static const unhandledError = '❌ Unhandled error occurred';
 
-  static const internalError = 'Internal error occurred';
+  static const internalError = '❌ Internal error occurred';
 
-  static String nullObject(String objectName) => '$objectName is null';
+  static String nullObject(String objectName) => '⚠️ $objectName is null';
 }
