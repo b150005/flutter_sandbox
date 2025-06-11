f25ffafa33e4236d056f20790860d14b146edd56
b150005 <b150005@outlook.jp>
2025-06-11 17:42:18 +0900

    🎨 リファクタリング

--- lib/ui/core/ui/adaptive_scaffold.dart
+++ lib/ui/core/ui/adaptive_scaffold.dart
@@ -17,65 +17,64 @@ class AdaptiveScaffold extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = ref.watch(appLocalizationsProvider);
 
-    switch (DesignPolicy.chooseNavigationLayout(context)) {
-      case NavigationLayout.bar:
-        return Scaffold(
-          body: _navigationShell,
-          bottomNavigationBar: NavigationBar(
-            selectedIndex: _navigationShell.currentIndex,
-            destinations: [
-              NavigationDestination(
-                icon: const Icon(Icons.home),
-                label: l10n.home,
-              ),
-              NavigationDestination(
-                icon: const Icon(Icons.science),
-                label: l10n.sample,
-              ),
-              NavigationDestination(
-                icon: const Icon(Icons.settings),
-                label: l10n.settings,
-              ),
-            ],
-            onDestinationSelected: (index) => _navigationShell.goBranch(
-              index,
-              initialLocation: index == _navigationShell.currentIndex,
-            ),
-          ),
-        );
-      case NavigationLayout.rail:
-        return Scaffold(
-          body: SafeArea(
-            child: Row(
-              children: [
-                NavigationRail(
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: const Icon(Icons.home),
-                      label: Text(l10n.home),
-                    ),
-                    NavigationRailDestination(
-                      icon: const Icon(Icons.science),
-                      label: Text(l10n.sample),
-                    ),
-                    NavigationRailDestination(
-                      icon: const Icon(Icons.settings),
-                      label: Text(l10n.settings),
-                    ),
-                  ],
-                  selectedIndex: _navigationShell.currentIndex,
-                  onDestinationSelected: (index) => _navigationShell.goBranch(
-                    index,
-                    initialLocation: index == _navigationShell.currentIndex,
+    return Scaffold(
+      body: SafeArea(
+        child: switch (DesignPolicy.chooseNavigationLayout(context)) {
+          NavigationLayout.bar => _navigationShell,
+          NavigationLayout.rail => Row(
+            children: [
+              NavigationRail(
+                destinations: [
+                  NavigationRailDestination(
+                    icon: const Icon(Icons.home),
+                    label: Text(l10n.home),
                   ),
-                  labelType: NavigationRailLabelType.all,
+                  NavigationRailDestination(
+                    icon: const Icon(Icons.science),
+                    label: Text(l10n.sample),
+                  ),
+                  NavigationRailDestination(
+                    icon: const Icon(Icons.settings),
+                    label: Text(l10n.settings),
+                  ),
+                ],
+                selectedIndex: _navigationShell.currentIndex,
+                onDestinationSelected: (index) => _navigationShell.goBranch(
+                  index,
+                  initialLocation: index == _navigationShell.currentIndex,
                 ),
-                const VerticalDivider(),
-                Expanded(child: _navigationShell),
-              ],
-            ),
+                labelType: NavigationRailLabelType.all,
+              ),
+              const VerticalDivider(),
+              Expanded(child: _navigationShell),
+            ],
           ),
-        );
-    }
+        },
+      ),
+      bottomNavigationBar:
+          DesignPolicy.chooseNavigationLayout(context) == NavigationLayout.bar
+          ? NavigationBar(
+              selectedIndex: _navigationShell.currentIndex,
+              destinations: [
+                NavigationDestination(
+                  icon: const Icon(Icons.home),
+                  label: l10n.home,
+                ),
+                NavigationDestination(
+                  icon: const Icon(Icons.science),
+                  label: l10n.sample,
+                ),
+                NavigationDestination(
+                  icon: const Icon(Icons.settings),
+                  label: l10n.settings,
+                ),
+              ],
+              onDestinationSelected: (index) => _navigationShell.goBranch(
+                index,
+                initialLocation: index == _navigationShell.currentIndex,
+              ),
+            )
+          : null,
+    );
   }
 }
