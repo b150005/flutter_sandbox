4263a7527e8e78f8a317a0ad11295d5aa863d21a
b150005 <b150005@outlook.jp>
2025-06-27 21:59:03 +0900

    🔥 Button の style を削除

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ import 'core/utils/extensions/nullable.dart';
 import 'core/utils/logging/log_message.dart';
 import 'core/utils/logging/logger.dart';
 import 'core/utils/logging/provider_observer.dart';
-import 'ui/core/themes/extensions/button_styles.dart';
 import 'ui/core/themes/extensions/card_text_styles.dart';
-import 'ui/core/themes/extensions/icon_button_styles.dart';
 import 'ui/core/themes/extensions/input_decoration_styles.dart';
 import 'ui/core/themes/extensions/text_styles.dart';
 import 'ui/core/ui/utils/scaffold_messenger.dart';
@@ -77,7 +75,6 @@ class App extends ConsumerWidget {
           TextStyles.light(context),
           CardTextStyles.light(context),
           InputDecorationStyles.light(context),
-          ButtonStyles.light(context),
         ],
       ),
       darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
@@ -85,7 +82,6 @@ class App extends ConsumerWidget {
           TextStyles.dark(context),
           CardTextStyles.dark(context),
           InputDecorationStyles.dark(context),
-          ButtonStyles.dark(context),
         ],
       ),
       localizationsDelegates: _localizationsDelegates,
