bd8a06ea87f81ccd8e6815628a42d76c64d9c015
b150005 <b150005@outlook.jp>
2025-09-17 23:11:19 +0900

    🔥♻️ サンプル画面の不要な定数値を削除し、 GridView の定数値の宣言方法を変更

---
+++ lib/core/config/constants/grid_dimensions.dart
@@ -0,0 +1,5 @@
+abstract final class GridDimensions {
+  const GridDimensions._();
+
+  static const double maxCrossAxisExtent = 480;
+}
--- lib/ui/sample/widgets/sample_screen.dart
+++ lib/ui/sample/widgets/sample_screen.dart
@@ -1,38 +1,5 @@
 part of '../../../core/routing/router.dart';
 
-enum SampleContent {
-  firebase(
-    path: FirebaseScreenRoute.absolutePath,
-    title: 'Firebase',
-    subtitle: 'Firebase Login',
-  ),
-  localStorage(
-    path: LocalStorageScreenRoute.absolutePath,
-    title: 'Local Storage',
-    subtitle: 'Local Storage Sandbox',
-  );
-
-  const SampleContent({
-    required this.path,
-    required this.title,
-    this.subtitle,
-    // ignore: unused_element_parameter
-    this.thumbnailPath,
-  });
-
-  final String path;
-  final String title;
-  final String? subtitle;
-  final String? thumbnailPath;
-
-  String description(l10n.AppLocalizations l10n) {
-    return switch (this) {
-      firebase => l10n.loginScreenDescription,
-      localStorage => l10n.localStorageScreenDescription,
-    };
-  }
-}
-
 class SampleScreenRoute extends GoRouteData with $SampleScreenRoute {
   static const path = '/sample';
 
@@ -44,14 +11,12 @@ class SampleScreenRoute extends GoRouteData with $SampleScreenRoute {
 class SampleScreen extends HookConsumerWidget {
   const SampleScreen({super.key});
 
-  static const double maxCrossAxisExtent = 480;
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final contents = ref.watch(sampleContentsProvider);
 
     return GridView.extent(
-      maxCrossAxisExtent: maxCrossAxisExtent,
+      maxCrossAxisExtent: GridDimensions.maxCrossAxisExtent,
       children: contents
           .map((content) => ContentCard(content: content))
           .toList(),
