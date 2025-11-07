f3bfa9c128bcb976c5fbffba4147fc4351c73185
b150005 <b150005@outlook.jp>
2025-11-07 22:53:28 +0900

    🎨 サインアウトボタンのリファクタリング

---
+++ lib/ui/core/ui/auth/sign_out_button.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../../core/config/constants/spacing.dart';
+import '../../../../core/config/constants/widget_keys.dart';
+import '../../../../core/utils/l10n/app_localizations.dart';
+import '../../../../data/repositories/firebase/auth/auth_repository.dart';
+import '../utils/preview/preview_mock_data.dart';
+import '../utils/preview/wrapper.dart';
+
+@Preview(name: 'Sign Out Button', wrapper: wrapper)
+Widget signOutButton() => ProviderScope(
+  overrides: [
+    // ignore: scoped_providers_should_specify_dependencies
+    firebaseAuthProvider.overrideWith(
+      (_) => PreviewMockData.mockFirebaseAuth,
+    ),
+  ],
+  child: Wrap(
+    spacing: Spacing.sm.dp,
+    children: [
+      const SignOutButton(),
+      SignOutButton.icon(),
+      SignOutButton.filled(),
+    ],
+  ),
+);
+
+@immutable
+class SignOutButton extends ConsumerWidget {
+  const SignOutButton({super.key});
+
+  factory SignOutButton.icon() => const _SignOutIconButton();
+
+  factory SignOutButton.filled() => const _SignOutFilledButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authRepository = ref.watch(authRepositoryProvider.notifier);
+    final l10n = ref.watch(appLocalizationsProvider);
+
+    return OutlinedButton.icon(
+      key: key ?? WidgetKeys.signOut,
+      onPressed: authRepository.signOut,
+      icon: const Icon(Icons.logout_outlined),
+      label: Text(l10n.signOut),
+    );
+  }
+}
+
+@immutable
+class _SignOutIconButton extends SignOutButton {
+  const _SignOutIconButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authRepository = ref.watch(authRepositoryProvider.notifier);
+
+    return IconButton.outlined(
+      key: key ?? WidgetKeys.signOut,
+      onPressed: authRepository.signOut,
+      icon: const Icon(Icons.logout_outlined),
+    );
+  }
+}
+
+@immutable
+class _SignOutFilledButton extends SignOutButton {
+  const _SignOutFilledButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authRepository = ref.watch(authRepositoryProvider.notifier);
+    final l10n = ref.watch(appLocalizationsProvider);
+
+    return FilledButton.icon(
+      onPressed: authRepository.signOut,
+      icon: const Icon(Icons.logout_outlined),
+      label: Text(l10n.signOut),
+    );
+  }
+}
