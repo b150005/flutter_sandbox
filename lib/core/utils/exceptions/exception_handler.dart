324bdcdd0cf49f56184acff5cb060299113cc91e
b150005 <b150005@outlook.jp>
2026-01-30 23:39:19 +0900

    ✨ App Check トークンエラー時のリフレッシュ機構を実装

--- lib/core/utils/exceptions/exception_handler.dart
+++ lib/core/utils/exceptions/exception_handler.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:multiple_result/multiple_result.dart';
@@ -10,7 +11,7 @@ import '../logging/log_message.dart';
 import '../logging/logger.dart';
 import 'app_exception.dart';
 
-class ExceptionHandler {
+abstract final class ExceptionHandler {
   const ExceptionHandler._();
 
   static Result<T, AppException> execute<T>(
@@ -45,6 +46,8 @@ class ExceptionHandler {
     FutureOr<T> Function() operation, {
     required AppLocalizations l10n,
     FutureOr<void> Function()? precheck,
+    FirebaseAppCheck? appCheck,
+    bool refreshesAppCheckToken = true,
   }) async {
     try {
       await precheck?.call();
@@ -57,6 +60,25 @@ class ExceptionHandler {
     } on FirebaseAuthException catch (error, stackTrace) {
       Logger.instance.e(error.message, error: error, stackTrace: stackTrace);
 
+      final canRetry =
+          error.isInvalidAppCheckToken &&
+          appCheck != null &&
+          refreshesAppCheckToken;
+
+      if (canRetry) {
+        Logger.instance.w(LogMessage.invalidAppCheckToken);
+
+        await _refreshesAppCheckToken(appCheck);
+
+        return executeAsync(
+          operation,
+          l10n: l10n,
+          precheck: precheck,
+          appCheck: appCheck,
+          refreshesAppCheckToken: false,
+        );
+      }
+
       return Result.error(error.toAppException(l10n));
     } on Exception catch (error, stackTrace) {
       Logger.instance.e(
@@ -66,6 +88,20 @@ class ExceptionHandler {
       );
 
       return const Result.error(AppException.unknown());
+    }
+  }
+
+  static Future<void> _refreshesAppCheckToken(FirebaseAppCheck appCheck) async {
+    try {
+      await appCheck.getToken(true);
+
+      Logger.instance.i(LogMessage.appCheckTokenRefreshSucceeded);
+    } on Exception catch (error, stackTrace) {
+      Logger.instance.e(
+        LogMessage.appCheckTokenRefreshFailed,
+        error: error,
+        stackTrace: stackTrace,
+      );
     }
   }
 }
