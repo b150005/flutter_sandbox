840a2df94c4f2fac74c214b0a26983b78e7fcbc0
b150005 <b150005@outlook.jp>
2026-03-20 00:01:55 +0900

    🔥 不要な import 文の削除

--- lib/core/utils/logging/logger.dart
+++ lib/core/utils/logging/logger.dart
@@ -2,7 +2,6 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:logger/logger.dart' as logger;
-import 'package:logging/logging.dart' as logging;
 import 'package:stack_trace/stack_trace.dart';
 
 import '../../config/capabilities/package_capability.dart';
